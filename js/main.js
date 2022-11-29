a152ca92d232c4bf8c9495ffdcb0d813f97e2328
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2022-11-29 16:06:29 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
-var swiper = new Swiper(".swiper", {
-  slidesPerView: "auto",
-  freeMode: false,
-  direction: "vertical",
-  loop: false,
-  loopedSlides: 8,
-  mousewheel: {
-    releaseOnEdges: true,
-  },
-});
+var windowW = $(window).width();
 
-swiper.on('slideChange', function () {
-  setTimeout(() => {
-    if ($('.swiper-slide-active').hasClass('nav_About')) {
-      $('.menu li a').removeClass('menuActive')
-      $('.menuAbout').addClass('menuActive')
-      //About Active
-    } else if ($('.swiper-slide-active').hasClass('nav_Clients')) {
-      $('.menu li a').removeClass('menuActive')
-      $('.menuClients').addClass('menuActive')
-      //Clients Active
-    } else if ($('.swiper-slide-active').hasClass('nav_Work')) {
-      $('.menu li a').removeClass('menuActive')
-      $('.menuWork').addClass('menuActive')
-      //Work Active
-    } else if ($('.swiper-slide-active').hasClass('nav_Contact')) {
-      $('.menu li a').removeClass('menuActive')
-      $('.menuContact').addClass('menuActive')
-      //Contact Active
-    }
+if (windowW > 490) {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: "auto",
+    freeMode: false,
+    direction: "vertical",
+    loop: false,
+    loopedSlides: 8,
+    mousewheel: {
+      releaseOnEdges: true,
+    },
   });
-}, 500);
+
+  swiper.on('slideChange', function () {
+    setTimeout(() => {
+      if ($('.swiper-slide-active').hasClass('nav_About')) {
+        $('.menu li a').removeClass('menuActive')
+        $('.menuAbout').addClass('menuActive')
+        //About Active
+      } else if ($('.swiper-slide-active').hasClass('nav_Clients')) {
+        $('.menu li a').removeClass('menuActive')
+        $('.menuClients').addClass('menuActive')
+        //Clients Active
+      } else if ($('.swiper-slide-active').hasClass('nav_Work')) {
+        $('.menu li a').removeClass('menuActive')
+        $('.menuWork').addClass('menuActive')
+        //Work Active
+      } else if ($('.swiper-slide-active').hasClass('nav_Contact')) {
+        $('.menu li a').removeClass('menuActive')
+        $('.menuContact').addClass('menuActive')
+        //Contact Active
+      }
+    });
+  }, 500);
+
+
+} else {
+  $('body').attr('style', 'overflow-x')
+  $('.swiperMobWrapper').removeClass('swiper-wrapper')
+
+  $('.vinarteImageFull').attr('src', 'images/vinarte_mob.png')
+  $('.vinarteDouble').attr('src', 'images/vinarteDouble.png')
+  $('.draftMobile').attr('src', 'images/draftMobile.png')
+  
+}
